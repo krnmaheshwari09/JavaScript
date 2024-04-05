0d30d9d1f23cbf1b769a6b5c6ce5f39017a4a1a2
Karan Maheshwari <krnmaheshwari2003@gmail.com>
2024-04-05 15:43:37 +0530

    Array.from is completed

---
+++ Array_from/Array_from.js
@@ -0,0 +1,28 @@
+// Array.from - Not on prototype
+
+// from - returns Array Object from an object
+// with a length property or an iterable object
+// from - turns array-like/ish into array - string, nodeList, Set
+
+const udemy = 'Udemy';
+
+// console.log(Array.from(udemy));
+
+const text = document.querySelectorAll('.text')
+const newText = Array.from(text).find((item) => item.textContent === 'person');
+// console.log(newText);
+
+const items = Array.from({length:120},(_, index) => {
+    return index;
+});
+
+const itemsPerPage = 9;
+const pages = Math.ceil(items.length/ itemsPerPage);
+
+const newItems = Array.from({length:pages}, (_, index) => {
+    const start = index*itemsPerPage;
+    const tempItems = items.slice(start, start + itemsPerPage);
+    return tempItems;
+})
+
+console.log(newItems)
